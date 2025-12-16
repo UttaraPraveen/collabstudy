c4bef81f226ea63028606ecc1c6c39b26758d9a1
Lekshmipriya34 <lekshmilechu3434@gmail.com>
2025-12-16 15:29:03 +0530

    welcome page

--- collaborative-study-room/src/App.jsx
+++ collaborative-study-room/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";     // ✅ NEW
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -7,7 +8,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* Welcome page */}
+        <Route path="/" element={<Welcome />} />
+
+        {/* Login page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Dashboard */}
         <Route
           path="/dashboard"
           element={
